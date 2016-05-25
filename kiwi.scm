17f271daca8020e80c0be7f9eba9035938ceeda2
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-05-25 10:16:08 +0200

    Stack-allocate rects, port rect helpers to scheme

--- kiwi.scm
+++ kiwi.scm
@@ -5,7 +5,8 @@
    init! process-events! paint! quit!
    tileset-surface-set!
    font-set!
-   rect release-rect! rect-center-in-parent! rect-fill-parent-horizontally! rect-x rect-y rect-w rect-h rect-x-set! rect-y-set! rect-w-set! rect-h-set!
+   rect rect-x rect-y rect-w rect-h rect-x-set! rect-y-set! rect-w-set! rect-h-set!
+   rect-center-in-parent! rect-center-in-parent-horizontally! rect-center-in-parent-vertically! rect-fill-parent-horizontally!
    widget-tileset-surface-set!
    frame
    scrollbox
@@ -43,18 +44,6 @@
 (define KW_LABEL_ALIGN_MIDDLE (foreign-value "KW_LABEL_ALIGN_MIDDLE" int))
 (define KW_LABEL_ALIGN_BOTTOM (foreign-value "KW_LABEL_ALIGN_BOTTOM" int))
 
-;; enum KW_RectHorizontalAlignment
-(define KW_RECT_ALIGN_HORIZONTALLY_NONE (foreign-value "KW_RECT_ALIGN_HORIZONTALLY_NONE" int))
-(define KW_RECT_ALIGN_LEFT (foreign-value "KW_RECT_ALIGN_LEFT" int))
-(define KW_RECT_ALIGN_CENTER (foreign-value "KW_RECT_ALIGN_CENTER" int))
-(define KW_RECT_ALIGN_RIGHT (foreign-value "KW_RECT_ALIGN_RIGHT" int))
-
-;; enum KW_RectVerticalAlignment
-(define KW_RECT_ALIGN_VERTICALLY_NONE (foreign-value "KW_RECT_ALIGN_VERTICALLY_NONE" int))
-(define KW_RECT_ALIGN_TOP (foreign-value "KW_RECT_ALIGN_TOP" int))
-(define KW_RECT_ALIGN_MIDDLE (foreign-value "KW_RECT_ALIGN_MIDDLE" int))
-(define KW_RECT_ALIGN_BOTTOM (foreign-value "KW_RECT_ALIGN_BOTTOM" int))
-
 ;;; foreign functions
 
 (define KW_CreateSDL2RenderDriver (foreign-lambda (c-pointer (struct "KW_RenderDriver")) "KW_CreateSDL2RenderDriver" (c-pointer (struct "SDL_Renderer")) (c-pointer (struct "SDL_Window"))))
@@ -70,18 +59,24 @@
 (define KW_SetFont (foreign-lambda void "KW_SetFont" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Font"))))
 (define KW_SetTilesetSurface (foreign-lambda void "KW_SetTilesetSurface" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Surface"))))
 (define KW_SetWidgetTilesetSurface (foreign-lambda void "KW_SetWidgetTilesetSurface" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Surface"))))
-(define KW_CreateFrame (foreign-lambda (c-pointer (struct "KW_Widget")) "KW_CreateFrame" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
-(define KW_CreateScrollbox (foreign-lambda (c-pointer (struct "KW_Widget")) "KW_CreateScrollbox" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
-(define KW_CreateLabel (foreign-lambda (c-pointer (struct "KW_Widget")) "KW_CreateLabel" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Widget")) c-string (c-pointer (struct "KW_Rect"))))
-(define KW_SetLabelIcon (foreign-lambda void "KW_SetLabelIcon" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
+
+(define KW_CreateFrame (foreign-lambda* (c-pointer (struct "KW_Widget")) (((c-pointer (struct "KW_GUI")) gui) ((c-pointer (struct "KW_Widget")) parent) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; C_return(KW_CreateFrame(gui, parent, &r));"))
+
+(define KW_CreateScrollbox (foreign-lambda* (c-pointer (struct "KW_Widget")) (((c-pointer (struct "KW_GUI")) gui) ((c-pointer (struct "KW_Widget")) parent) (int x) (int y) (int w) (int h))" KW_Rect r = { x, y, w, h }; C_return(KW_CreateScrollbox(gui, parent, &r));"))
+
+(define KW_CreateLabel (foreign-lambda* (c-pointer (struct "KW_Widget")) (((c-pointer (struct "KW_GUI")) gui) ((c-pointer (struct "KW_Widget")) parent) (c-string text) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; C_return(KW_CreateLabel(gui, parent, text, &r));"))
+(define KW_SetLabelIcon (foreign-lambda* void (((c-pointer (struct "KW_Widget")) label) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; KW_SetLabelIcon(label, &r);"))
 (define KW_SetLabelAlignment (foreign-lambda void "KW_SetLabelAlignment" (c-pointer (struct "KW_Widget")) (enum "KW_LabelHorizontalAlignment") int (enum "KW_LabelVerticalAlignment") int))
-(define KW_CreateButton (foreign-lambda (c-pointer (struct "KW_Widget")) "KW_CreateButton" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Widget")) c-string (c-pointer (struct "KW_Rect"))))
-(define KW_CreateEditbox (foreign-lambda (c-pointer (struct "KW_Widget")) "KW_CreateEditbox" (c-pointer (struct "KW_GUI")) (c-pointer (struct "KW_Widget")) c-string (c-pointer (struct "KW_Rect"))))
+
+(define KW_CreateButton (foreign-lambda* (c-pointer (struct "KW_Widget")) (((c-pointer (struct "KW_GUI")) gui) ((c-pointer (struct "KW_Widget")) parent) (c-string text) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; C_return(KW_CreateButton(gui, parent, text, &r));"))
+
+(define KW_CreateEditbox (foreign-lambda* (c-pointer (struct "KW_Widget")) (((c-pointer (struct "KW_GUI")) gui) ((c-pointer (struct "KW_Widget")) parent) (c-string text) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; C_return(KW_CreateEditbox(gui, parent, text, &r));"))
 (define KW_SetEditboxFont (foreign-lambda void "KW_SetEditboxFont" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Font"))))
-(define KW_GetWidgetGeometry (foreign-lambda void "KW_GetWidgetGeometry" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
-(define KW_GetWidgetAbsoluteGeometry (foreign-lambda void "KW_GetWidgetAbsoluteGeometry" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
-(define KW_SetWidgetGeometry (foreign-lambda void "KW_SetWidgetGeometry" (c-pointer (struct "KW_Widget")) (c-pointer (struct "KW_Rect"))))
-(define KW_RectCenterInParent (foreign-lambda void "KW_RectCenterInParent" (c-pointer (struct "KW_Rect")) (c-pointer (struct "KW_Rect"))))
+
+(define KW_GetWidgetGeometry (foreign-lambda* void (((c-pointer (struct "KW_Widget")) widget) ((c-pointer int) x) ((c-pointer int) y) ((c-pointer int) w) ((c-pointer int) h)) "KW_Rect r; KW_GetWidgetGeometry(widget, &r); *x = r.x, *y = r.y, *w = r.w, *h = r.h;"))
+(define KW_GetWidgetAbsoluteGeometry (foreign-lambda* void (((c-pointer (struct "KW_Widget")) widget) ((c-pointer int) x) ((c-pointer int) y) ((c-pointer int) w) ((c-pointer int) h)) "KW_Rect r; KW_GetWidgetAbsoluteGeometry(widget, &r); *x = r.x, *y = r.y, *w = r.w, *h = r.h;"))
+(define KW_SetWidgetGeometry (foreign-lambda* void (((c-pointer (struct "KW_Widget")) widget) (int x) (int y) (int w) (int h)) "KW_Rect r = { x, y, w, h }; KW_SetWidgetGeometry(widget, &r);"))
+
 (define KW_AddWidgetMouseOverHandler (foreign-lambda void "KW_AddWidgetMouseOverHandler" (c-pointer (struct "KW_Widget")) (function void ((c-pointer (struct "KW_Widget"))))))
 (define KW_AddWidgetMouseLeaveHandler (foreign-lambda void "KW_AddWidgetMouseLeaveHandler" (c-pointer (struct "KW_Widget")) (function void ((c-pointer (struct "KW_Widget"))))))
 (define KW_AddWidgetMouseDownHandler (foreign-lambda void "KW_AddWidgetMouseDownHandler" (c-pointer (struct "KW_Widget")) (function void ((c-pointer (struct "KW_Widget")) int))))
@@ -90,39 +85,13 @@
 (define KW_AddWidgetDragStopHandler (foreign-lambda void "KW_AddWidgetDragStopHandler" (c-pointer (struct "KW_Widget")) (function void ((c-pointer (struct "KW_Widget")) int int))))
 (define KW_AddWidgetDragHandler (foreign-lambda void "KW_AddWidgetDragHandler" (c-pointer (struct "KW_Widget")) (function void ((c-pointer (struct "KW_Widget")) int int int int))))
 
-;;; foreign rect helpers
-
-(define KW_CreateRect
-  (foreign-lambda* (c-pointer (struct "KW_Rect")) ((int x) (int y) (int w) (int h))
-    "KW_Rect *r = calloc(sizeof(KW_Rect), 1);"
-    "r->x = x; r->y = y; r->w = w; r->h = h;"
-    "C_return(r);"))
-
-(define KW_Rect->x (foreign-lambda* int (((c-pointer (struct "KW_Rect")) r)) "C_return(r->x);"))
-(define KW_Rect->y (foreign-lambda* int (((c-pointer (struct "KW_Rect")) r)) "C_return(r->y);"))
-(define KW_Rect->w (foreign-lambda* int (((c-pointer (struct "KW_Rect")) r)) "C_return(r->w);"))
-(define KW_Rect->h (foreign-lambda* int (((c-pointer (struct "KW_Rect")) r)) "C_return(r->h);"))
-
-(define KW_Rect->x-set! (foreign-lambda* void (((c-pointer (struct "KW_Rect")) r) (int x)) "r->x = x;"))
-(define KW_Rect->y-set! (foreign-lambda* void (((c-pointer (struct "KW_Rect")) r) (int y)) "r->y = y;"))
-(define KW_Rect->w-set! (foreign-lambda* void (((c-pointer (struct "KW_Rect")) r) (int w)) "r->w = w;"))
-(define KW_Rect->h-set! (foreign-lambda* void (((c-pointer (struct "KW_Rect")) r) (int h)) "r->h = h;"))
-
-(define KW_RectFillParentHorizontally
-  (foreign-lambda* void (((c-pointer (struct "KW_Rect")) outer) (pointer-vector rects_vector) (int rects_vector_length) (u32vector weights_vector) (int weights_vector_length) (unsigned-int count) (int padding) ((enum "KW_RectVerticalAlignment") align))
-    "KW_Rect *rects[rects_vector_length];"
-    "memcpy(rects, rects_vector, rects_vector_length * sizeof(KW_Rect *));"
-    "unsigned int weights[weights_vector_length];"
-    "memcpy(weights, weights_vector, weights_vector_length * sizeof(unsigned int));"
-    "KW_RectFillParentHorizontally(outer, rects, weights, count, padding, align);"))
-
 ;;; auxiliary records
 
 (define-record driver pointer)
 (define-record surface pointer)
 (define-record font pointer)
 (define-record gui pointer)
-(define-record rect pointer)
+(define-record rect x y w h)
 (define-record widget handlers pointer)
 
 ;;; generic handlers
@@ -241,89 +210,53 @@
 
 ;;; rects
 
-(define (rect x y w h)
-  (if-let (rect* (KW_CreateRect x y w h))
-    (set-finalizer! (make-rect rect*) release-rect!)
-    (abort (oom-error 'rect))))
-
-(define (release-rect! rect)
-  (and-let* ((rect* (rect-pointer rect)))
-    (free rect*)
-    (rect-pointer-set! rect #f)))
-
-(define (rect-center-in-parent! outer inner)
-  (and-let* ((inner* (rect-pointer inner))
-             (outer* (rect-pointer outer)))
-    (KW_RectCenterInParent inner* outer*)))
-
-(define (rect-fill-parent-horizontally! outer rects weights count padding valign)
-  (and-let* ((outer* (rect-pointer outer)))
-    (let* ((rect-pointers (map rect-pointer rects))
-           (rects-vector (apply pointer-vector rect-pointers))
-           (rects-vector-length (pointer-vector-length rects-vector))
-           (weights-vector (list->u32vector weights))
-           (weights-vector-length (u32vector-length weights-vector))
-           (valign (case valign
-                     ((none) KW_RECT_ALIGN_VERTICALLY_NONE)
-                     ((top) KW_RECT_ALIGN_TOP)
-                     ((middle) KW_RECT_ALIGN_MIDDLE)
-                     ((bottom) KW_RECT_ALIGN_BOTTOM)
-                     (else
-                      (abort (usage-error "Invalid vertical alignment value"
-                                          'rect-fill-parent-horizontally!))))))
-      (KW_RectFillParentHorizontally outer* rects-vector rects-vector-length
-                                     weights-vector weights-vector-length
-                                     count padding valign))))
-
-(define (rect-x rect)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->x rect*)))
-
-(define (rect-x-set! rect x)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->x-set! rect* x)))
+(define rect make-rect)
 
 (define rect-x (getter-with-setter rect-x rect-x-set!))
-
-(define (rect-y rect)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->y rect*)))
-
-(define (rect-y-set! rect y)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->y-set! rect* y)))
-
 (define rect-y (getter-with-setter rect-y rect-y-set!))
-
-(define (rect-w rect)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->w rect*)))
-
-(define (rect-w-set! rect w)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->w-set! rect* w)))
-
 (define rect-w (getter-with-setter rect-w rect-w-set!))
-
-(define (rect-h rect)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->h rect*)))
-
-(define (rect-h-set! rect h)
-  (and-let* ((rect* (rect-pointer rect)))
-    (KW_Rect->h-set! rect* h)))
-
 (define rect-h (getter-with-setter rect-h rect-h-set!))
+
+(define (rect-center-in-parent-horizontally! parent inner)
+  (set! (rect-x inner) (- (/ (rect-w parent) 2) (/ (rect-w inner) 2))))
+
+(define (rect-center-in-parent-vertically! parent inner)
+  (set! (rect-y inner) (- (/ (rect-h parent) 2) (/ (rect-h inner) 2))))
+
+(define (rect-center-in-parent! parent inner)
+  (rect-center-in-parent-horizontally! parent inner)
+  (rect-center-in-parent-vertically! parent inner))
+
+(define (rect-fill-parent-horizontally! parent rects weights count padding valign)
+  (let* ((total (foldr + 0 weights))
+         (base (/ (- (rect-w parent) (* padding (add1 (length weights)))) total)))
+    (let loop ((current 0)
+               (rects rects)
+               (weights weights))
+      (when (pair? weights)
+        (let ((inner (car rects))
+              (weight (car weights)))
+          (set! (rect-x inner) (+ current padding))
+          (set! (rect-w inner) (* base weight))
+          (case valign
+            ((top) (set! (rect-y inner) 0))
+            ((middle) (rect-center-in-parent-vertically! parent inner))
+            ((bottom) (set! (rect-y inner) (- (rect-h parent) (rect-h inner)))))
+          (loop (+ (rect-x inner) (rect-w inner))
+                (cdr rects) (cdr weights)))))))
 
 ;;; widgets
 
 (define widget-table (make-hash-table))
 
 (define (define-widget type gui parent geometry proc)
-  (and-let* ((gui* (gui-pointer gui))
-             (geometry* (rect-pointer geometry)))
-    (let ((parent* (and parent (widget-pointer parent))))
-      (if-let (widget* (proc gui* parent* geometry*))
+  (and-let* ((gui* (gui-pointer gui)))
+    (let ((parent* (and parent (widget-pointer parent)))
+          (x (rect-x geometry))
+          (y (rect-y geometry))
+          (w (rect-w geometry))
+          (h (rect-h geometry)))
+      (if-let (widget* (proc gui* parent* x y w h))
         (let* ((handlers (make-hash-table eqv? eqv?-hash))
                (widget (make-widget handlers widget*)))
           (hash-table-set! widget-table widget* widget)
@@ -343,12 +276,15 @@
 
 (define (label gui parent text geometry)
   (define-widget 'label gui parent geometry
-    (cut KW_CreateLabel <> <> text <>)))
+    (cut KW_CreateLabel <> <> text <> <> <> <>)))
 
 (define (label-icon-set! label clip)
-  (and-let* ((label* (widget-pointer label))
-             (clip* (rect-pointer clip)))
-    (KW_SetLabelIcon label* clip*)))
+  (and-let* ((label* (widget-pointer label)))
+    (let ((x (rect-x clip))
+          (y (rect-y clip))
+          (w (rect-w clip))
+          (h (rect-h clip)))
+      (KW_SetLabelIcon label* x y w h))))
 
 (define (label-alignment-set! label halign hoffset valign voffset)
   (and-let* ((label* (widget-pointer label)))
@@ -370,11 +306,11 @@
 
 (define (button gui parent text geometry)
   (define-widget 'button gui parent geometry
-    (cut KW_CreateButton <> <> text <>)))
+    (cut KW_CreateButton <> <> text <> <> <> <>)))
 
 (define (editbox gui parent text geometry)
   (define-widget 'editbox gui parent geometry
-    (cut KW_CreateEditbox <> <> text <>)))
+    (cut KW_CreateEditbox <> <> text <> <> <> <>)))
 
 (define (editbox-font-set! editbox font)
   (and-let* ((editbox* (widget-pointer editbox))
@@ -383,17 +319,26 @@
 
 (define (widget-geometry widget #!optional absolute?)
   (and-let* ((widget* (widget-pointer widget)))
-    (let* ((geometry (rect 0 0 0 0))
-           (geometry* (rect-pointer geometry)))
-      (if absolute?
-          (KW_GetWidgetAbsoluteGeometry widget* geometry*)
-          (KW_GetWidgetGeometry widget* geometry*))
+    (let ((geometry (rect 0 0 0 0))
+          (proc (if absolute? KW_GetWidgetAbsoluteGeometry KW_GetWidgetGeometry)))
+      (let-location ((x int)
+                     (y int)
+                     (w int)
+                     (h int))
+        (proc widget* (location x) (location y) (location w) (location h))
+        (set! (rect-x geometry) x)
+        (set! (rect-y geometry) y)
+        (set! (rect-w geometry) w)
+        (set! (rect-h geometry) h))
       geometry)))
 
 (define (widget-geometry-set! widget geometry)
-  (and-let* ((widget* (widget-pointer widget))
-             (geometry* (rect-pointer geometry)))
-    (KW_SetWidgetGeometry widget* geometry*)))
+  (and-let* ((widget* (widget-pointer widget)))
+    (let ((x (rect-x geometry))
+          (y (rect-y geometry))
+          (w (rect-w geometry))
+          (h (rect-h geometry)))
+      (KW_SetWidgetGeometry widget* x y w h))))
 
 (define widget-geometry (getter-with-setter widget-geometry widget-geometry-set!))
 
